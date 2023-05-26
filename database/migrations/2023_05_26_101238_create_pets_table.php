c31a328bca470735f18ce066c1dc16ef8d174bc0
marikadiblasio <marbla.albram@gmail.com>
2023-05-26 12:17:48 +0200

    migration pets completed

---
+++ database/migrations/2023_05_26_101238_create_pets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('type', 50);
+            $table->string('race', 50);
+            $table->string('name', 50);
+            $table->dateTime('data_of_birth');
+            $table->boolean('adopted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+};
